437009d1e19abadb6c06258bee4c2542a7986601
Vitaliy Artemov <olivera507224@yandex.ru>
2022-04-06 21:03:56 +0300

    add migrations

---
+++ src/Task.Context/Migrations/20220406175506_CadetAndOfficerRemoveDivisionForeignKey.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task.Context.Migrations
+{
+    public partial class CadetAndOfficerRemoveDivisionForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cadets_Divisions_DivisionId",
+                table: "Cadets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Officers_Divisions_DivisionId",
+                table: "Officers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Officers_DivisionId",
+                table: "Officers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cadets_DivisionId",
+                table: "Cadets");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Officers_DivisionId",
+                table: "Officers",
+                column: "DivisionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cadets_DivisionId",
+                table: "Cadets",
+                column: "DivisionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cadets_Divisions_DivisionId",
+                table: "Cadets",
+                column: "DivisionId",
+                principalTable: "Divisions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Officers_Divisions_DivisionId",
+                table: "Officers",
+                column: "DivisionId",
+                principalTable: "Divisions",
+                principalColumn: "Id");
+        }
+    }
+}
